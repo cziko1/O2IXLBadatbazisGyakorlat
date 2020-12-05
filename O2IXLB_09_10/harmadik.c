324e61b6ef5734fed8383f4556caa4c20f97a97a
cziko1 <72433224+cziko1@users.noreply.github.com>
2020-12-05 15:07:34 +0100

    Add files via upload

---
+++ O2IXLB_09_10/harmadik.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pelda_2(char *fnev1, char *fnev2)
+{
+	FILE *fp1, *fp2;char ch;int pos;
+	if ((fp1 = fopen(fnev1, "r")) == NULL)
+	{
+		printf("\nNem lehet megnyitni a fajlt.");
+		return;
+	}else{
+		printf("\nFajl megnyitva masolasra...\n ");
+	}
+	fp2 = fopen(fnev2, "w");
+	fseek(fp1, 0L, SEEK_END);
+	pos = ftell(fp1);
+	fseek(fp1, 0L, SEEK_SET);
+	while (pos--)
+	{    
+	ch = fgetc(fp1)    
+	fputc(ch, fp2);
+	}
+	fclose(fp1);
+	fclose(fp2);
+}
